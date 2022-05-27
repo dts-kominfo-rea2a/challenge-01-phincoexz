1a701723685d76091a13e521d51d1525e176bc52
phincoexz <32565269+phincoexz@users.noreply.github.com>
2022-05-27 16:47:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,129 +1,91 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-// const firstUser: {
-//     name: string;
-//     gender: string;
-//     age: string;
-//     email: string;
-//     favoriteColor: string[];
-//     isHavePet: string;
-//     education: ({
-//         name: string;
-//         city: string;
-//         graduate: number;
-//     } | {
-//         name: string;
-//         city: string;
-//         graduate?: undefined;
-//     })[];
-//     favoriteRestaurant: string[];
-// }
 
+// Color List
+const yellow = "Yellow";
+const pink = "Pink";
+const white = "White";
+const purple = "Purple";
+const blue = "Blue";
+const black = "Black";
+const grey = "Grey";
+
+// Restaurant List
+const bento = "Bento";
+const sushi = "Sushi";
+const pancake = "Pancake";
+const eggy = "Eggy";
+const tempura = "Tempura";
+const padang = "Padang";
+const tteok = "Tteok";
+const katsu = "Katsu";
+const geprek = "Geprek";
+
+let monicasColor = new Set();
+monicasColor.add(yellow);
+monicasColor.add(pink);
+monicasColor.add(white);
+monicasColor.add(purple);
+
+let wendysColor = new Set();
+wendysColor.add(blue);
+wendysColor.add(black);
+wendysColor.add(grey);
+
+let monicasResto = new Set();
+monicasResto.add(bento);
+monicasResto.add(sushi);
+monicasResto.add(pancake);
+monicasResto.add(eggy);
+monicasResto.add(tempura);
+monicasResto.add(bento);
+monicasResto.add(eggy);
+monicasResto.add(padang);
+monicasResto.add(tteok);
+monicasResto.add(sushi);
+monicasResto.add(sushi);
+
+let wendysResto = new Set();
+wendysResto.add(tempura);
+wendysResto.add(bento);
+wendysResto.add(sushi);
+wendysResto.add(pancake);
+wendysResto.add(padang);
+wendysResto.add(katsu);
+wendysResto.add(geprek);
+wendysResto.add(pancake);
+wendysResto.add(eggy);
+
+// Start Test
 const firstUser = {
     name: "Monica",
-    gender: "Female",
+    gender: "female",
     age: "17",
     email: "monica@dingdong.com",
-    favoriteColor: [
-        "Yellow",
-        "Pink",
-        "White",
-        "Purple"
-    ],
+    favoriteColor: [...monicasColor],
     isHavePet: "Yes",
     education: [
-        {
-            name: "SD 01",
-            city: "Jakarta",
-            graduate: 2016,
-        },
-        {
-            name: "SMP 02",
-            city: "Jakarta",
-            graduate: 2019,
-        },
-        {
-            name: "SMA 03",
-            city: "Tangerang",
-        },
+        { name: "SD 01", city: "Jakarta", graduate: "2016" },
+        { name: "SMP 02", city: "Jakarta", graduate: "2019" },
+        { name: "SMA 03", city: "Tangerang" },
     ],
-    favoriteRestaurant: [
-        "Bento",
-        "Sushi",
-        "Pancake",
-        "Eggy",
-        "Tempura",
-        "Bento",
-        "Eggy",
-        "Padang",
-        "Tteok",
-        "Sushi",
-        "Sushi"
-    ],
-
+    favoriteRestaurant: [...monicasResto],
 };
-// const secondUser: {
-//     name: string;
-//     dender: string;
-//     age: number;
-//     email: string;
-//     favoriteColor: string[];
-//     isHavePet: string;
-//     education: ({
-//         name: string;
-//         city: string;
-//         graduate: number;
-//     } | {
-//         name: string;
-//         city: string;
-//         graduate?: undefined;
-//     })[];
-//     favoriteRestaurant: string[];
-// }
 const secondUser = {
     name: "Wendy",
-    dender: "Male",
-    age: 23,
+    gender: "male",
+    age: "23",
     email: "wendy@dingdong.com",
-    favoriteColor: [
-        "Blue", 
-        "Black", 
-        "Grey"
-    ],
+    favoriteColor: [...wendysColor],
     isHavePet: "No",
     education: [
-        {
-            name: "SD 02",
-            city: "Jakarta",
-            graduate: 2010,
-        },
-        {
-            name: "SMP 03",
-            city: "Bogor",
-            graduate: 2013,
-        },
-        {
-            name: "SMA 01",
-            city: "Surabaya",
-            graduate: 2016,
-        },
-        {
-            name: "Universitas Maju",
-            city: "Tangerang",
-        },
+        { name: "SD 02", city: "Jakarta", graduate: "2010" },
+        { name: "SMP 03", city: "Bogor", graduate: "2013" },
+        { name: "SMA 01", city: "Surabaya", graduate: "2016" },
+        { name: "Universitas Maju", city: "Tangerang" },
     ],
-    favoriteRestaurant: [
-        "Tempura", 
-        "Bento", 
-        "Sushi", 
-        "Pancake", 
-        "Padang", 
-        "Katsu", 
-        "Geprek", 
-        "Pancake", 
-        "Eggy"
-    ],
+    favoriteRestaurant: [...wendysResto],
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
@@ -140,5 +102,5 @@ function main() {
 main();
 
 module.exports = {
-    users
+    users,
 };
